e8f15ece1f2b36d0929390421a414f6c0c8d3b77
Cristelm03 <fmoralesl5@miumg.edu.gt>
2025-11-15 00:28:37 -0600

    FEAT:Libreria con fuentes de las APIS

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const apiClientes = axios.create({
   baseURL: "http://localhost:8080/api",
   headers: { "Content-Type": "application/json" }
-});
+}); 
 
 export const apiProveedores = axios.create({
   baseURL: "http://localhost:8081/api",
